50f8933ea240a07738eb03185a1e00b6603683d0
Vic Chimenti <victor.chimenti@gmail.com>
2025-08-25 08:51:24 -0700

    import existing page layout

---
+++ pageLayout-be-legendary-id9690915-footer.php
@@ -0,0 +1,57 @@
+                    </article>
+                </main>
+            </div>
+            <!-- .page-wrapper -->
+            <t4 type="navigation" name="Return Back to top" id="1083" />
+            <t4 type="navigation" name="Return Sock" id="982" />
+            <t4 type="navigation" name="Return Common Footer HTML" id="950" />
+
+        </div>
+        <!-- .page -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>   
+        <!-- jquery.min.js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha512-hJSZLjaUow3GsiAkjUBMxN4eaFysMaBvg7j6mkBeo219ZGmSe1eVhKaJJAj5GzGoD0j0Gr2/xNDzjeecdg+OCw==" crossorigin="anonymous"></script>
+        <!-- popper.min.js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js" integrity="sha512-qOBWNAMfkz+vXXgbh0Wz7qYSLZp6c14R0bZeVX2TdQxWpuKr6yHjBIM69fcF8Ve4GUX6B6AKRQJqiiAmwvmUmQ==" crossorigin="anonymous"></script>
+        <!-- typehead.min.js -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js" integrity="sha512-zT3zHcFYbQwjHdKjCu6OMmETx8fJA9S7E6W7kBeFxultf75OPTYUJigEKX58qgyQMi1m1EgenfjMXlRZG8BXaw==" crossorigin="anonymous"></script>
+        <!-- handlebars.min.js -->
+        <script nomodule src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+        <!-- es6-promise.auto.min.js -->
+        <!-- Partial Scripts -->
+        <script src="https://dxp-us-search.funnelback.squiz.cloud/s/resources/seattleu~sp-search/_default/js/stencils.js"></script>
+        <script src="https://dxp-us-search.funnelback.squiz.cloud/s/resources/seattleu~sp-search/_default/js/handlebars-helpers.js"></script>
+        <!-- End Partial Scripts -->
+        <script src="<t4 type='media' id='8723392' formatter='path/*' />" defer></script>
+        <!-- flats.min.js -->
+        <script src="<t4 type='media' id='6907524' formatter='path/*' />" defer></script>
+        <!-- modernizr.js -->
+        <script src="<t4 type='media' id='8718323' formatter='path/*' />" defer></script>
+        <!-- app.min.js -->
+        <script src="<t4 type='media' id='6907526' formatter='path/*' />" defer></script>
+        <!-- touchevents.js -->
+        <script src="<t4 type='media' id='6930621' formatter='path/*' />" defer></script>
+        <!-- emergency-alert.js -->
+        <script src="<t4 type='media' id='7022505' formatter='path/*' />" defer></script>
+        <!-- t4-extras.js -->
+        <script src="https://su-search.vercel.app/js/SessionService.js" fetchpriority="high"></script>
+        <!-- Session Service Manager -->
+        <script src="https://su-search.vercel.app/integration.js" defer></script>
+        <!-- Frontend Search API Integration -->
+        <script src="https://su-search.vercel.app/search-page-autocomplete.js" async></script>
+        <!-- Frontend Search Autocomplete -->
+        <script type="module" src="https://su-search.vercel.app/js/search-index.js"></script>
+        <!-- Search Module Integration -->
+
+
+        
+        <t4 type="navigation" name="Return Gallery JS" id="956" />
+        <t4 type="navigation" name="Return Event Promo JS" id="965" />
+        <t4 type="navigation" name="Contact Listing JS" id="1029" />
+        <t4 type="navigation" name="V10 Top Content Custom JS" id="1017" />
+        <t4 type="navigation" name="V10 - Custom Scripts" id="1015" />
+        <t4 type="navigation" name="Return Scripts for End of Body" id="980" />
+
+
+    </body>
+</html>
---
+++ pageLayout-be-legendary-id9690915-header.php
@@ -0,0 +1,86 @@
+<t4 type="navigation" name="v10 Access Control - PHP" id="1021" /><t4 type="navigation" name="Return PHP Head" id="977" /><t4 type="media" id="7506257" formatter="inline/*" />
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="id" content="<t4 type='navigation' name='Get Current Section ID' id='886' />" />
+    <meta property="publisher" @type="Organization" name="Seattle University">
+    <t4 type="navigation" name="Return Canonical Tags" id="975" />
+    <t4 type="navigation" name="V10 - Recursive Title" id="971" />
+    <t4 type="navigation" name="Return Meta Tags" id="973" />
+    <t4 type="navigation" name="Fulltext Meta Navigation" id="1019" />
+    <t4 type="meta" id="54" />
+    <t4 type="meta" id="22" />
+    <meta name="generator" content="Terminalfour">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="siteid" content="us">
+    <meta name="countryid" content="US">
+    <meta name="country" content="United States">
+    <meta name="Language" content="en-US">
+    <meta name="googlebot" content="all">
+    <meta name="robots" content="all">
+
+    <!-- Preconnects -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"  />
+
+    <!-- Preloads -->
+    <link
+        rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;1,500;1,800&amp;family=Oswald:wght@300;700&amp;family=Roboto+Slab:wght@200;300&amp;display=swap"
+        as="style"
+        crossorigin
+    />
+    <link
+        rel="preload"
+        href="/media/seattle-university/site-assets/css/theme.min.css"
+        as="style"
+    />
+
+    <!-- Custom Head Code -->
+    <t4 type="navigation" name="V10 - Custom Header Code" id="1014" />
+    
+    <!-- Nav Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href='<t4 type="media" id="6913741" formatter="path/*" />'>
+    <link rel="icon" type="image/png" sizes="32x32" href='<t4 type="media" id="6913745" formatter="path/*" />'>
+    <link rel="icon" type="image/png" sizes="16x16" href='<t4 type="media" id="6913749" formatter="path/*" />'>
+    <link rel="manifest" href='<t4 type="media" id="6913805" formatter="path/*" />'>
+    <link rel="shortcut icon" href='<t4 type="media" id="6913753" formatter="path/*" />'>
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content='<t4 type="media" id="6913806" formatter="path/*" />'>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;1,500;1,800&amp;family=Oswald:wght@300;700&amp;family=Roboto+Slab:wght@200;300&amp;display=swap" />
+    <t4 type="media" id="6907520" formatter="cssver/*" />
+    <!-- app.css -->
+    <t4 type="media" id="8739839" formatter="cssver/*" />
+    <!-- theme.min.css -->
+    <t4 type="media" id="6907522" formatter="cssver/*" />
+    <!-- all.css -->
+    <t4 type="media" id="7022512" formatter="cssver/*" />
+    <!-- t4-extras.css -->
+    <t4 type="media" id="8953952" formatter="cssver/*" />
+    <!-- funnelback.css -->
+
+    <!-- Custom -->
+    <t4 type="navigation" name="Return Scripts for Head" id="978" />
+    <t4 type="navigation" name="V10 - Custom CSS Zone" id="1013" />
+
+</head>
+
+<body class="">
+
+    <t4 type="navigation" name="Return Scripts for Start of Body" id="979" />
+
+    <div class="page page--page <t4 type='navigation' name='Return Page Class' id='966' />">
+
+        <t4 type="navigation" name="Return Site Wide Notices" id="970" />
+
+        <div class="emergencynotice" aria-live="assertive" data-t4-emergency-notices="<t4 type='media' id='6930620' formatter='path/*' />"></div>
+
+        <t4 type="navigation" name="Return Common Header HTML" id="940" />
+
+        <div class="page-wrapper">
+            <main id="main-content" class="main-content">
+                <article<t4 type="navigation" name="Return Article Class" id="967" />>
