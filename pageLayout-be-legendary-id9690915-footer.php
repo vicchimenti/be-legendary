619d9a2b76517774b8a495e2572b09ea68443bad
Vic Chimenti <victor.chimenti@gmail.com>
2025-08-25 10:30:55 -0700

    footer: scrub legacy styles

--- pageLayout-be-legendary-id9690915-footer.php
+++ pageLayout-be-legendary-id9690915-footer.php
@@ -1,33 +1,6 @@
-                    </article>
-                </main>
-            </div>
-            <!-- .page-wrapper -->
+
             <t4 type="navigation" name="Return Back to top" id="1083" />
-
         </div>
-        <!-- .page -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>   
-        <!-- jquery.min.js -->
-        <script src="<t4 type='media' id='8723392' formatter='path/*' />" defer></script>
-        <!-- flats.min.js -->
-        <script src="<t4 type='media' id='6907524' formatter='path/*' />" defer></script>
-        <!-- modernizr.js -->
-        <script src="<t4 type='media' id='8718323' formatter='path/*' />" defer></script>
-        <!-- app.min.js -->
-        <script src="<t4 type='media' id='6907526' formatter='path/*' />" defer></script>
-        <!-- touchevents.js -->
-        <script src="<t4 type='media' id='6930621' formatter='path/*' />" defer></script>
-        <!-- emergency-alert.js -->
-        <script src="<t4 type='media' id='7022505' formatter='path/*' />" defer></script>
-        <!-- t4-extras.js -->
-        
-        <t4 type="navigation" name="Return Gallery JS" id="956" />
-        <t4 type="navigation" name="Return Event Promo JS" id="965" />
-        <t4 type="navigation" name="Contact Listing JS" id="1029" />
-        <t4 type="navigation" name="V10 Top Content Custom JS" id="1017" />
-        <t4 type="navigation" name="V10 - Custom Scripts" id="1015" />
         <t4 type="navigation" name="Return Scripts for End of Body - Be Legendary" id="1123" />
-
-
     </body>
 </html>
